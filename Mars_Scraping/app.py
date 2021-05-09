146360dc06b1da931e404a49b324744e0ad1f19b
Jessica Veilleux <jessicaveilleux@Jessicas-Air.attlocal.net>
2021-05-09 16:16:54 -0500

    fixing file order

--- Mars_Scraping/app.py
+++ Mars_Scraping/app.py
@@ -12,7 +12,7 @@ mongo = PyMongo(app)
 
 def index():
    mars = mongo.db.mars.find_one()
-   return render_template("index.html", mars=mars)
+   return render_template("index.html" , mars=mars)
 
 @app.route("/scrape")
 
@@ -25,4 +25,3 @@ def scrape():
 if __name__ == "__main__":
    app.run()
 
-   
